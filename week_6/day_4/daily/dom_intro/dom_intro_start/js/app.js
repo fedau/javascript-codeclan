3070912c9cc6036877159e6b8002a0045d4905d4
fedau <fedau.d@hotmail.com>
2022-12-15 13:12:01 +0000

    dom intro

--- week_6/day_4/daily/dom_intro/dom_intro_start/js/app.js
+++ week_6/day_4/daily/dom_intro/dom_intro_start/js/app.js
@@ -1,1 +1,24 @@
+console.log('app loaded', window);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const title = document.querySelector('h1');
+    title.textContent = "Javascript says, BOO";
+
+    const welcomeParagraph = document.querySelector('#welcome-paragraph')
+    console.log(welcomeParagraph);
+    console.dir(welcomeParagraph);
+
+    const redElement = document.querySelector('li.red')
+    redElement.textContent = "RED!!!!"
+    redElement.classList.add('bold');
+
+    const allLiElements = document.querySelectorAll('li')
+    console.log(allLiElements);
+
+    const newListItem = document.createElement('li')
+    newListItem.textContent = 'Purple';
+    newListItem.classList.add('purple');
+    const list = document.querySelector('ul')
+    list.appendChild(newListItem)
+})
 
