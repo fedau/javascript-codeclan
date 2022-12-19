d3a6637bebe50400c90640d33bcb5657d0247a5e
fedau <fedau.d@hotmail.com>
2022-12-19 09:50:28 +0000

    homework

--- week_6/day_4/daily/lab_events/lab_events_start/js/app.js
+++ week_6/day_4/daily/lab_events/lab_events_start/js/app.js
@@ -1,3 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+
+  const handleFormSubmit =function(event){
+    event.preventDefault()
+    // console.log(event.target.title.value);
+ 
+    // const resultCard = document.querySelector('#reading-list')
+    // resultCard.textContent = `${event.target.title.value} ${event.target.author.value}  ${event.target.category.value}`m.textContent = `${event.target.title.value} ${event.target.author.value}  ${event.target.category.value}`;
+
+    const newListItem = document.createElement('li')
+    newListItem.textContent = `${event.target.title.value} ${event.target.author.value}  ${event.target.category.value}`;
+    newListItem.classList.add('#reading-list');
+    const list = document.querySelector('ul')
+    list.appendChild(newListItem)
+
+
+    document.getElementById("new-item-form").reset();
+
+
+  }
+
+  const form = document.querySelector("#new-item-form")
+  form.addEventListener('submit', handleFormSubmit)
+
+  // the function
+  
+  const handleButtonClick = function(){
+    // console.log('button has been clicked');
+    document.querySelector('ul').clear()
+  }
+// the event
+
+  const button = document.querySelector('#button')
+  button.addEventListener('click', handleButtonClick);
 })
---
+++ week_6/weekend/weekend_hw/react_intro/hello_world/src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  const name = "Fedau"
+  return (
+    <div>
+      <h1 className="title"> hello {name}! You be rocking it today </h1>
+
+    </div>
+
+  );
+}
+
+export default App;
